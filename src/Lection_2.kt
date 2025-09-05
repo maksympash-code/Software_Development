0c83db29103d5b3a33d2a70af68a4f45dfb7507f
Maksym Pashchenko <maksympash@knu.ua>
2025-09-05 16:34:31 +0300

    Lection_2

---
+++ src/Lection_2.kt
@@ -0,0 +1,143 @@
+fun main() {
+//    println("Hello World!")
+
+//    var a: Int = 100
+//    val b: UInt = a
+//
+//    a += 100
+//    a++
+//    ++a
+
+//    val a = true
+//    val b = false
+//    println(a)
+//    println(b)
+//    println(a && b)
+//    println(a || b)
+//    println(!a)
+
+//    val a = 100
+//    val b = 100
+//    println(a == b)
+//    println(a != b)
+//
+//    val s1 = "Hello World"
+//    val s2 = "Hello World"
+//
+//    println(s1 === s2)
+//
+//    val a = "Hello"
+//    val s3 = "$a World"
+//
+//    println(s3 === s1)
+
+
+//    val a: Int = 10
+//    val b: Long = a.toLong()
+//
+//    println(a)
+//    println(b)
+
+//    val s = "123weff"
+//    val a = s.toIntOrNull()
+//
+//    println(a)
+
+//    val n = 10
+//    val text = n.toString()
+//    println(text)
+//    println(n)
+
+//    var n: Number = 42
+//    n = 45.0f
+//    println(n)
+//    val m: Int = n as Int // ClassCastException
+//
+//    println("$n, $m")
+
+//    var i = 100
+//    println(i)
+//
+//    if (i == 100){
+//        var i = 555
+//        println(i)
+//    }
+//
+//    println(i)
+
+//    val s: String = "a"
+//    val c: Char = 'a'
+
+//    var a = "Andrii"
+//    var b = "Viacheslav"
+//
+//    var s = "Hello ${a + " and " + b}!"
+//    println(s)
+
+//    val arr = arrayOf(1, 2, 3, 11, 5, 6, 7, 8, 9, 10)
+//
+////    for (i in arr){
+////        println(i)
+////    }
+//    arr.sort()
+//    println(arr.contentToString())
+//    println(arr[1])
+//
+//    arr[1] = 34
+//    println(arr.contentToString())
+//
+//    println(3 !in arr)
+
+
+//    val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle")
+//    shapes.add("triangle 2")
+//    shapes.add("circle 2")
+//    println(shapes)
+//
+//    ////// something code
+//
+//    val readOnlyShapes: List<String> = shapes
+////    readOnlyShapes.add() // compile error
+//
+//    val newShapes: MutableList<String> = shapes as MutableList<String> // TeamLead broke hands from it
+//    newShapes.add("triangle 3")
+//    newShapes.add("circle 3")
+//    println(newShapes)
+
+
+
+//    val shapes: MutableList<String> = mutableListOf("triangle", "square", "circle")
+//    shapes.add("triangle 2")
+//    shapes.add("circle 2")
+//
+//    for (shape in shapes){
+//        println(shape)
+//    }
+
+
+//    var i = 1
+//    while(i < 10){
+//        println(i)
+//        i++
+//    }
+
+
+
+
+    val check = true
+
+    val d = if (check){
+        1
+    }
+    else{
+        2
+    }
+
+//    val d: Int
+//    if (check){
+//        d = 1
+//    }
+//    else{
+//        d = 2
+//    }
+}
