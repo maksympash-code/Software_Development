be70f878eca6767ab27cfb49fd8bff8a6a94174f
Maksym Pashchenko <maksympash@knu.ua>
2025-09-08 17:16:51 +0300

    Lesson_10(comparison_operators)

---
+++ After_Classes/Lesson_10.kt
@@ -0,0 +1,18 @@
+fun main() {
+//    val isBigger: Boolean = !(2 > 1)
+//    println(isBigger)
+//    println(2 < 1)
+//    println(2 == 1)
+//    println(1 == 1)
+//    println(2 != 1)
+//    println(2 >= 2)
+//    println(2 >= 3)
+//    println(2 <= 3)
+
+    val tempWater = -2
+
+    if (tempWater <= 0)
+        println("Water is cold")
+    else
+        println("Water is ok")
+}
