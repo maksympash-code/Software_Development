eb9fbaa9ac8bf72b0c94369ea1bc37991a82c460
Maksym Pashchenko <maksympash@knu.ua>
2025-09-08 16:24:33 +0300

    Lesson_6 Comments

---
+++ After_Classes/Lesson_6.kt
@@ -0,0 +1,17 @@
+
+/**
+ * gnrgeg
+ * wfnwefw
+ * enwf ew
+ * It`s a multiline comment
+*/
+
+// point of view in our program
+fun main() {
+    // It`s a comment.
+
+    // It`s my Number
+    val myNumber: Int = 10
+    // function println with parameter myNumber
+    println("Number: $myNumber")
+}
