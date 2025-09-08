89eb8f5431c8065767231c2c8c507ef58295232a
Maksym Pashchenko <maksympash@knu.ua>
2025-09-08 16:48:24 +0300

    Lesson_8 (if, else if, else)

---
+++ After_Classes/Lesson_8.kt
@@ -0,0 +1,19 @@
+fun main() {
+    val phoneLang: String = "oo" // Ukrainian language
+
+    if (phoneLang == "uk"){
+        println("Привіт, світ!")
+    }
+    else if(phoneLang == "fr"){
+        println("Bonjour.")
+    }
+    else{
+        println("Hello, World!")
+    }
+
+    if (true){
+        println("true")
+    }
+
+    println("End")
+}
