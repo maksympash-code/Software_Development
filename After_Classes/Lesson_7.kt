96cfe5c2ba551928bdbbdb518c9c455aa0e3d62e
Maksym Pashchenko <maksympash@knu.ua>
2025-09-08 16:36:00 +0300

    Lesson_7 (arithmetic_operations)

---
+++ After_Classes/Lesson_7.kt
@@ -0,0 +1,29 @@
+fun main() {
+    // +
+    // -
+    // *
+    // /
+    // %
+
+    val x: Int = 3
+    val y: Int = 2
+
+    println(x + y)
+    println(x - y)
+    println(x * y)
+
+    val xAsDouble: Double = x.toDouble()
+    val yAsDouble: Double = y.toDouble()
+
+    val res: Double = xAsDouble / yAsDouble
+
+    println(res)
+
+    println(12 % 10)
+    println(25 % 10)
+
+
+    val result: Int = (3 - 2) * 2 / 1
+    println(result)
+
+}
