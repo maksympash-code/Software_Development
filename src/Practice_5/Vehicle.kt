a4b81ab5763784e0acae9271affafb2bce29d88a
Maksym Pashchenko <maksympash@knu.ua>
2025-09-21 10:05:03 +0300

    Practice_5

---
+++ src/Practice_5/Vehicle.kt
@@ -0,0 +1,7 @@
+package Practice_5
+
+data class Vehicle(var mark: String = "",
+                   var model: String = "",
+                   var year: Int = 0,
+                   var license_plate: String = "") {
+}
